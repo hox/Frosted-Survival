3512039f017c1e81349af2ce89857211aec7e5ae
Caeser <ehernandez@evistaresources.com>
2018-02-16 14:15:20 -0500

    [Added] Started work on kits

---
+++ src/com/FrostedIsles/Comp/Kits.java
@@ -0,0 +1,14 @@
+package com.FrostedIsles.Comp;
+
+import org.bukkit.entity.Player;
+
+public class Kits {
+
+	public Kits() {
+		
+	}
+	
+	public static void giveKit(Player p, String kitname) {
+		
+	}
+}
