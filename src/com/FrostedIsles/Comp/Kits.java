c138d1e50dab114f39f0da02c932dd6bc678d9df
Jacon <jacon500@gmail.com>
2018-02-22 17:40:27 -0600

    [Fix] Kits now work as intended

--- src/com/FrostedIsles/Comp/Kits.java
+++ src/com/FrostedIsles/Comp/Kits.java
@@ -1,41 +1,29 @@
 package com.FrostedIsles.Comp;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-public class Kits {
-	private static ConfigurationManager kits;
-	
-	public Kits() {
-		kits = new ConfigurationManager();
-		kits.setup(new File(Main.plugin.getDataFolder(), "kits.yml"));
-	}
-	
+public class Kits {	
 	public static void giveKit(Player p, String kitname) {
-		ConfigurationSection section = kits.data.getConfigurationSection(kitname);
+		ConfigurationSection section = Main.kits.data.getConfigurationSection(kitname);
 		
 		if (section != null) {
-			Inventory inv = p.getInventory();
 			Set<String> items = section.getValues(true).keySet();
 			
 			for (String n : items) {
-				if (section.isItemStack(n)) { // Key goes with an item stack
-					inv.addItem(section.getItemStack(n));
-				}
+				p.getInventory().addItem(section.getItemStack(n));
 			}
 		}
 	}
 	
 	@SuppressWarnings("deprecation")
 	public static void addKit(String name, String[] items) {
-		ConfigurationSection section = kits.data.createSection(name);
+		ConfigurationSection section = Main.kits.data.createSection(name);
 		
 		for (Integer i = 0; i < items.length; i++) {
 			String[] item = items[i].split(",");
@@ -51,15 +39,15 @@ public class Kits {
 			section.set(i.toString(), stack);
 		}
 		
-		kits.saveData();
+		Main.kits.saveData();
 	}
 	
 	public static void removeKit(String name) {
-		Object kit = kits.data.get(name);
+		Object kit = Main.kits.data.get(name);
 		
 		if (kit != null) {
-			kits.data.set(name, null);
-			kits.saveData();
+			Main.kits.data.set(name, null);
+			Main.kits.saveData();
 		}
 	}
 	
@@ -67,15 +55,15 @@ public class Kits {
 		List<String> k = new ArrayList<>();
 		k.add("Available kits: ");
 
-		if (kits.data.getValues(true).isEmpty()) {
+		if (Main.kits.data.getValues(true).isEmpty()) {
 			k.add("There are no kits!");
 		} else {
-			for (String h : kits.data.getValues(true).keySet()) {
+			for (String h : Main.kits.data.getValues(false).keySet()) {
 				k.add(h);
 			}
 		}
 
-		String toSend = Util.buildMessage((String[]) k.toArray(), ", ");
+		String toSend = Util.buildMessage((String[]) k.toArray(new String[k.size()]), ", ");
 		Util.sendMsgNoPre(p, toSend);
 	}
 }
